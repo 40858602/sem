c3df452bae265af38b469ed63c2430fa9cffae9d
40858602 <40858602@live.napier.ac.uk>
2026-09-22 15:30:55 +0630

    Add MongoDB connection code to App.java

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -5,25 +5,23 @@ import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoCollection;
 import org.bson.Document;
 
-public class App
-{
-    public static void main(String[] args)
-    {
-        // Connect to MongoDB on local system - we're using port 27000
-        MongoClient mongoClient = new MongoClient("mongo-dbserver", 27017);
-        // Get a database - will create when we use it
-        MongoDatabase database = mongoClient.getDatabase("mydb");
-        // Get a collection from the database
+public class App {
+    public static void main(String[] args) {
+        // Connect to MongoDB on local system using port 27000
+        MongoClient mongoClient = new MongoClient("localhost", 27000);
+
+        // Get database (will be created if it doesn't exist)
+        MongoDatabase database = mongoClient.getDatabase("test");
+
+        // Get collection
         MongoCollection<Document> collection = database.getCollection("test");
-        // Create a document to store
-        Document doc = new Document("name", "Kevin Sim")
-                .append("class", "DevOps")
-                .append("year", "2024")
-                .append("result", new Document("CW", 95).append("EX", 85));
-        // Add document to collection
+
+        // Create a document and insert it
+        Document doc = new Document("name", "Fred Flintstone")
+                .append("company", "Slate Rock and Gravel");
         collection.insertOne(doc);
 
-        // Check document in collection
+        // Retrieve and print the document
         Document myDoc = collection.find().first();
         System.out.println(myDoc.toJson());
     }
